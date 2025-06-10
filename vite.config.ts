1cfa4aaf8023802312c042a65597b745d0094b59
Jo-Ann Bachhuber <jo-annbachhuber@MacBookAir.fritz.box>
2025-06-10 21:20:47 +0200

    feat: Update vite config for /de base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/kinder-creations-site/',
+  base: '/de',
   plugins: [vue()],
   resolve: {
     alias: {
